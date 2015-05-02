3d2f431e7a32f0b049b8782b569cf11978d1360d
cjmatson <matson.cj@gmail.com>
2015-05-02 14:48:47 -0500

    Changes to challenge.js

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,38 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(actual, guess) {
+	if (actual > guess) {
+		return "Too low";
+	}
+	else if (actual == guess) {
+		return "You got it!";
+	}
+	else {
+		return "Too high";
+	}
+};
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(a, b, c, d) {
+	if ((a == 3 || a == 5 || a == 7) && (b == 2) && (5 <= c && 100 >= c) && (9 > d || d > 20)) {
+		return "correct";
+}
+	else {
+		return "incorrect";
+	}
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+	if (item == "MacBook Air" && money >= 999) {
+		return true;
+	}
+	else if (item == "MacBook Pro" && money >= 1299) {
+		return true;
+	}
+	else if (item == "Mac Pro" && money >= 2499) {
+		return true;
+	}
+	else if (item == "Apple Sticker" && money >= 1) {
+		return true;
+	}
+	else {
+		return false;
+	}
+};
